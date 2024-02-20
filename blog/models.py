c0fe3e411a3852a0c5deffcfb55384d4ea7a0b6f
tokushigeshuya <narihare1130@gmail.com>
2024-02-21 00:10:34 +0900

    [add]モデルの追加

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Blog(models.Model):
+class Post(models.Model):
   # ブログ記事のタイトル（200文字制限）
   title = models.CharField("タイトル",max_length=200)
   # ブログ記事の内容
-  content = models.TextField("内容",blank=True)
+  content = models.TextField("本文",blank=True)
   # 作成日時 auto_now_add=Trueでインスタンスの作成(DBにINSERT)する度に更新される
-  created_at = models.DateTimeField(auto_now_add=True)
+  created_at = models.DateTimeField("作成日",auto_now_add=True)
   # 更新日時 auto_now=Trueの場合はモデルインスタンスを保存する度に現在の時間で更新
-  updated_at = models.DateTimeField(auto_now=True)
+  updated_at = models.DateTimeField("更新日",auto_now=True)
   # boolean型の真偽値（記事の公開設定）
-  is_published = models.BooleanField(help_text='公開の場合はTrue')
+  is_published = models.BooleanField("公開設定",default=False)
+
+  # 管理画面で記事のタイトルを表示させる
+  def __str__(self):
+    return self.title
